20d113437cf21fed3c7ff0cf439a0fa984edbb02
varun-2024 <v.talwar2024@gmail.com>
2025-07-06 13:12:15 +0100

    Add Awards, Education, Hero, Pricing, and Stats components; remove Navbar component

---
+++ frontend/src/landing_page/home/Awards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Awards() {
+  return <h1 className="">Awards</h1>;
+}
+
+export default Awards;
---
+++ frontend/src/landing_page/home/Education.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Education() {
+  return <h1 className="">Education</h1>;
+}
+
+export default Education;
