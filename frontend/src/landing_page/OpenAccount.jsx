362378043c309779fe56c91d642e5c59158dd49d
varun-2024 <v.talwar2024@gmail.com>
2025-07-06 20:18:46 +0100

    feat: Create OpenAccount component with structured layout and signup button

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function OpenAccount() {
-  return <h1 className="">OpenAccount</h1>;
+  return (
+    <div className="container p-5 mt-5 mb-5">
+      <div className="row mb-5 text-center">
+        <h1 className="mt-5">Open a Zerodha account</h1>
+        <p className="">
+          Modern platforms and apps, £0 investments, and flat ₹20 intraday and
+          F&O trades.
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default OpenAccount;
