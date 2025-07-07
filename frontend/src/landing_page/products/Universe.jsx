1bf9a8448219523d0ed98c080d72ad92b08f36a4
varun-2024 <v.talwar2024@gmail.com>
2025-07-07 15:33:55 +0100

    feat: Update Hero and Universe components with new pricing details and improved layout

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -11,28 +11,40 @@ function Universe() {
         </p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
+          <img src="media/images/smallcaseLogo.png" style={{ width: "75%" }} />
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img
+            src="media/images/streakLogo.png"
+            style={{ width: "55%", marginTop: "5px" }}
+          />
+          <p className="text-small text-muted">Algo & Strategy platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img
+            src="media/images/sensibullLogo.svg"
+            style={{ width: "75%", marginTop: "18px" }}
+          />
+          <p className="text-small text-muted">Options Trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img
+            src="media/images/zerodhaFundhouse.png"
+            style={{ width: "75%" }}
+          />
+          <p className="text-small text-muted">Asset Management</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img
+            src="media\images\goldenpiLogo.png"
+            style={{ width: "55%", marginTop: "15px" }}
+          />
+          <p className="text-small text-muted">Bonds Trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="media\images\dittoLogo.png" style={{ width: "55%" }} />
+          <p className="text-small text-muted">Insurance</p>
         </div>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
